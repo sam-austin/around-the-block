33d061a44fa7a6ddaed5d311950c125495e11820
“Potato <samson.park1@gmail.com>
2021-02-06 18:56:42 -0500

    initial commit

---
+++ server/src/config/getDatabaseUrl.cjs
@@ -0,0 +1,11 @@
+const getDatabaseUrl = (nodeEnv) => {
+  return (
+    {
+      development: "postgres://postgres:postgres@localhost:5432/engage_development",
+      test: "postgres://postgres:postgres@localhost:5432/engage_test",
+      e2e: "postgres://postgres:postgres@localhost:5432/engage_e2e",
+    }[nodeEnv] || process.env.DATABASE_URL
+  );
+};
+
+module.exports = getDatabaseUrl;
